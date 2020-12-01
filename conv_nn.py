a4bb842a8c753f55b58a9cba23a286eb683e0f80
Hugo <hugo.re83@gmail.com>
2020-12-01 15:32:57 +0100

    Initial commit: 1 Layer conv network - Skeleton + Few utils functions

---
+++ conv_nn.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+from utils import convert_to_4_chan, show_image_mask_real, convert_to_1_chan, TrainDataset
+
+
+class CNNSEG(nn.Module): # Define your model
+    def __init__(self):
+        super(CNNSEG, self).__init__()
+        #torch.nn.Conv2d(in_channels: int, out_channels: int, kernel_size: Union[T, Tuple[T, T]])
+        self.conv = nn.Conv2d(1, 4, 3)
+        # fill in the constructor for your model here
+    def forward(self, x):
+        # fill in the forward function for your model here
+        return self.conv(x)
+
+if __name__ == '__main__':
+
+    model = CNNSEG() # We can now create a model using your defined segmentation model
+
+    lr = 0.00001
+    Loss = nn.L1Loss()
+
+    optimizer = optim.SGD(model.parameters(), lr=lr)
+
+    data_path = './data/train'
+    num_workers = 1
+    batch_size = 10
+    train_set = TrainDataset(data_path)
+    training_data_loader = DataLoader(dataset=train_set, num_workers=num_workers, batch_size=batch_size, shuffle=True)
+
+    loss_array = []
+
+    for iteration, sample in enumerate(training_data_loader):
+        print("Iteration", str(iteration + 1) + "/" + str(len(training_data_loader)))
+
+        img, mask = sample
+        # Converting the mask image [10,96,96] to a matrix [10,1,94,94]. The 94 is to match the forward function output.
+        mask_converted = convert_to_4_chan(mask[:, 1:-1, 1:-1])
+
+        # Adding a new dimension to the img
+        img = img[:, None, :, :]
+
+        # Calculating the expected mask
+        out = model.forward(img)
+
+        # Display the predicted mask (after conversion to the image format)
+        img_1_chan = convert_to_1_chan(out)
+        # Display the image and the mask
+        show_image_mask_real(img[0, ...].squeeze(), mask[0, ...].squeeze(), img_1_chan[0].squeeze())
+        plt.pause(0.5)
+
+        # Calculating the loss
+        loss = Loss(out, mask_converted)
+        loss_array.append(loss)
+
+        loss.backward()
+        optimizer.step()
+
+    plt.figure()
+    plt.plot(loss_array)
+
---
+++ utils.py
@@ -0,0 +1,73 @@
+from matplotlib import pyplot as plt
+import torch
+import torch.utils.data as data
+import cv2
+import os
+from glob import glob
+
+
+def show_image_mask_real(img, mask, real, cmap='gray'):  # visualisation
+    fig = plt.figure(figsize=(5, 5))
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(img, cmap=cmap)
+    plt.axis('off')
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(mask, cmap=cmap)
+    plt.axis('off')
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(real, cmap=cmap)
+
+
+
+class TrainDataset(data.Dataset):
+    def __init__(self, root=''):
+        super(TrainDataset, self).__init__()
+        self.img_files = glob(os.path.join(root, 'image', '*.png'))
+        self.mask_files = []
+        for img_path in self.img_files:
+            basename = os.path.basename(img_path)
+            self.mask_files.append(os.path.join(root, 'mask', basename[:-4] + '_mask.png'))
+
+    def __getitem__(self, index):
+        img_path = self.img_files[index]
+        mask_path = self.mask_files[index]
+        data = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+        label = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
+        return torch.from_numpy(data).float(), torch.from_numpy(label).float()
+
+    def __len__(self):
+        return len(self.img_files)
+
+
+class TestDataset(data.Dataset):
+    def __init__(self, root=''):
+        super(TestDataset, self).__init__()
+        self.img_files = glob(os.path.join(root, 'image', '*.png'))
+
+    def __getitem__(self, index):
+        img_path = self.img_files[index]
+        data = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+        return torch.from_numpy(data).float()
+
+    def __len__(self):
+        return len(self.img_files)
+
+def convert_to_4_chan(img):
+    ishape = img.shape
+    result = torch.zeros((ishape[0], 4, ishape[1], ishape[2]))
+    result[:, 0, :, :] = torch.where(img == 0, 1, 0)
+    result[:, 1, :, :] = torch.where(img == 1, 1, 0)
+    result[:, 2, :, :] = torch.where(img == 2, 1, 0)
+    result[:, 3, :, :] = torch.where(img == 3, 1, 0)
+    return result
+
+
+def convert_to_1_chan(img):
+    ishape = img.shape
+    result = torch.zeros((ishape[0], ishape[1], ishape[2]))
+    result = torch.argmax(img, dim=1)
+
+    return result
