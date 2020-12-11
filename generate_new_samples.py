9e3965af243a5c5125ae133f5060156d6d40a346
Hugo <hugo.re83@gmail.com>
2020-12-11 16:47:05 +0100

    Ajout d'un generateur de nouvelles données

--- generate_new_samples.py
+++ generate_new_samples.py
@@ -16,14 +16,16 @@ if __name__ == '__main__':
         basename = os.path.basename(img_path)
         mask_files.append(os.path.join(root, 'mask', basename[:-4] + '_mask.png'))
 
+    print("Generating images:")
+    total_img = 4*len(img_files)
     for i in range(len(img_files)):
-        img = cv2.imread(img_files[i])
-        mask = cv2.imread(mask_files[i])
+        img = cv2.imread(img_files[i], cv2.IMREAD_UNCHANGED)
+        mask = cv2.imread(mask_files[i], cv2.IMREAD_UNCHANGED)
         cv2.imwrite(os.path.join(generated_folder, "image", 'gen' + str(i * 4) + '.png'), img)
         cv2.imwrite(os.path.join(generated_folder, "mask", 'gen' + str(i * 4) + '_mask.png'), mask)
         for j in range(3):
-            print(i*3+j)
+            print(str(i*4+j+2) + "/" + str(total_img))
             cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE, img)
             cv2.rotate(mask, cv2.ROTATE_90_CLOCKWISE, mask)
             cv2.imwrite(os.path.join(generated_folder, "image", 'gen' + str(i * 4 + j + 1) + '.png'), img)
-            cv2.imwrite(os.path.join(generated_folder, "mask", 'gen' + str(i * 3 + j) + '_mask.png'), mask)
+            cv2.imwrite(os.path.join(generated_folder, "mask", 'gen' + str(i * 4 + j + 1) + '_mask.png'), mask)
